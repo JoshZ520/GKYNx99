e0b63a8dd1849a8ce6eb71725b9517141ece05e4
Josh <joshuazob@gmail.com>
2025-11-19 12:01:15 -0700

    updates

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
-require('dotenv').config();
+// Load environment variables from .env file in development
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
